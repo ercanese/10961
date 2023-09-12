681d66d8ac47e74b6dd6af0625e482a45dee5425
Ecan ESE <ercan.ese@bilgeadam.com>
2023-09-12 11:14:13 +0300

    Day2 Examples 1

--- Day2.ps1
+++ Day2.ps1
@@ -19,3 +19,34 @@ Get-Help -Name Invoke-Sqlcmd -Full
 
 
 #-------------------------------------------
+
+#AD üzerinde bir adet HelpDesk adında bir grup oluşturalım.
+Get-Command -Verb * -Noun *Group*
+Get-Command -Verb * -Noun ADGroup
+
+Get-Help -Name New-ADGroup -Full
+
+New-ADGroup -DisplayName "Helpdesk" -Description "IT Grubudur" -GroupCategory Security -GroupScope Global -Name Helpdesk 
+Get-ADGroup -Identity HElpdesk
+
+
+#AD üzerinde herkes kendi ismiyle bir kullanıcı oluştursun.
+
+Get-Command -Verb New -Noun "*User*"
+New-ADUser -Name "ErcanEse" -SamAccountName "ercan.ese" -Department "IT"
+Get-ADUser -Identity ercan.ese
+
+#Oluşturdugunuz kullanıcıyı helpdesk grubuna ekleyelim.
+Get-Command -Verb * -Noun "ADGroup*"
+
+Add-ADGroupMember -Identity helpdesk -Members ercan.ese
+
+
+
+#Olusturgumuz kullanıcının Departman bilgisini ITM olarak set edelim.
+Get-Command -Verb * -Noun ADUser
+Set-ADUser -Identity ercan.ese -Department ITM
+Get-ADUser -Identity ercan.ese -Properties *
+
+#Grubunun üyelerini çekelim powershell ile.
+Get-ADGroupMember -Identity helpdesk
